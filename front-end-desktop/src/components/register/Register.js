409210007451d0427de79e70e70a444fb9b1c381
Rayan <rayan.mazri@epitech.eu>
2025-04-02 22:23:05 +0200

    [ADD] call api by endpoint

--- front-end-desktop/src/components/register/Register.js
+++ front-end-desktop/src/components/register/Register.js
@@ -1,15 +1,39 @@
 import React, { useState } from 'react';
 import logo from '../../assets/imperiatec_logo.jpeg';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Register.css';
 
 const Register = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
-  const onClic = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
-    console.log('try to connect with:', { email, password });
+
+    const payload = { username, email, password };
+
+    try {
+      const response = await fetch('http://localhost:8000/api/accounts/register/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      });
+
+      if (response.ok) {
+        navigate('/login');
+      } else {
+        const errorData = await response.json();
+        setError(errorData.error || 'Error');
+      }
+    } catch (err) {
+      console.error('Registration failed', err);
+      setError('Registration failed');
+    }
   };
 
   return (
@@ -18,7 +42,18 @@ const Register = () => {
       <img src={logo} alt="Logo" />
       </a>
       <h2>Sign up</h2>
-      <form className="register-form" onClic={onClic}>
+      <form className="register-form" onSubmit={onSubmit}>
+        <div className="input-group">
+          <input
+            type="username"
+            id="username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            name="username"
+            required
+          />
+        </div>
         <div className="input-group">
           <input
             type="email"
